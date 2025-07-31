c06f10d952bd4ec333a4c96bd64808ba3a396989
apexlawndivya <djanga@apexlawncompany.com>
2025-07-31 19:48:24 +0530

    redeploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const isProd = process.env.NODE_ENV === 'production';
+
+const nextConfig = {
+  output: 'export', // required for static export
+  basePath: isProd ? '/prana-healthcare' : '',
+  assetPrefix: isProd ? '/prana-healthcare/' : '',
+  reactStrictMode: true,
+};
 
 export default nextConfig;
